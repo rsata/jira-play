4ffce5cb4526556ad02da9d7bc79b8680681e996
Reid Sata <rsata@rubiconnt.localnet>
2016-12-20 09:26:32 -0800

    rm unneeded tostring

--- jira-server-test/routes/index.js
+++ jira-server-test/routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 var request = require('request');
 
 
-// const query = 'project = DEP AND resolution = unresolved AND assignee = rsata'
-const query = 'resolution = Done AND project = DEP AND createdDate > "2016/01/01"';
-// const query = 'resolution = Done AND project = SS AND createdDate > "2016/01/01"'
-// const query = 'resolution = Unresolved AND project = DEP'
+const query = 'project = DEP AND resolution = unresolved AND assignee = rsata';
+// const query = 'resolution = Done AND project = DEP AND createdDate > "2016/01/01"';
+// const query = 'resolution = Done AND project = SS AND createdDate > "2016/01/01"';
+// const query = 'resolution = Unresolved AND project = DEP';
 
 const fields = 'assignee,status,resolution,timetracking';
 
 function jqlQuery(query) {
-  return encodeURIComponent(query).toString();
+  return encodeURIComponent(query);
 }
 
 router.get('/api/get', (req, res, next) => {
